5041955a58a0f98733ba5354d3bf681526095ab4
edom18 <edo.m18@gmail.com>
2015-07-27 21:28:43 +0900

    first commit.

---
+++ Assets/ExplosionRagdoll.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class ExplosionRagdoll : MonoBehaviour {
+
+	#region publicVariables
+	public float m_force = 500f;
+	public float m_radius = 100f;
+
+	// For replacing game object in explosion.
+	public GameObject m_ragdoll;
+	#endregion
+
+	private bool isEnd = false;
+
+	void Start () {
+
+	}
+	
+	void Update () {
+		if (isEnd) {
+			return;
+		}
+
+		if (Input.GetMouseButton(0)) {
+			isEnd = true;
+
+			GameObject ragdoll = Instantiate(m_ragdoll, transform.position, transform.rotation) as GameObject;
+
+			GameObject.Destroy(gameObject);
+
+			Component[] rigidbodies = ragdoll.GetComponentsInChildren<Rigidbody>();
+			foreach (Rigidbody rigidbody in rigidbodies) {
+				rigidbody.AddExplosionForce(m_force, Vector3.zero, m_radius);
+			}
+		}
+	}
+}
